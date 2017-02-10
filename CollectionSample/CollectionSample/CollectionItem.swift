c30a2165f46bcdc6c718948fe185f430eaaf0b66
Appinventiv <appinventiv@Appinventivs-Mac-mini.local>
2017-02-10 13:53:27 +0530

    Collection Item Cell Creation

---
+++ CollectionSample/CollectionSample/CollectionItem.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionItem.swift
+//  CollectionSample
+//
+//  Created by Appinventiv on 10/02/17.
+//  Copyright © 2017 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class CollectionItem: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
